fa3878d7d8d2ae87cf06d67fa844dfe8a2f6080b
estevaoMG <estevao.gouveia@hotmail.com>
2025-10-16 19:28:10 -0300

    Criação de schemas e models - Entidades Categoria e Centro de Treinamento

--- atleta/models.py
+++ atleta/models.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
-from sqlalchemy import Datetime, Float, Integer, String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Datetime, Float, ForeignKey, Integer, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from contrib.models import BaseModel
 
@@ -17,3 +17,9 @@ class AtletaModel(BaseModel):
     altura: Mapped[float] = mapped_column(Float, nullable=False)
     sexo: Mapped[str] = mapped_column(String(1), nullable=False)
     created_at: Mapped[datetime] = mapped_column(Datetime, nullable=False)
+    centro_treinamento: Mapped["CentroTreinamentoModel"] = relationship(
+        back_populates="atleta"
+    )
+    centro_treinamento_id: Mapped[int] = mapped_column(
+        Integer, ForeignKey("centros_treinamento.pk_id")
+    )
