4dc0c44ea341513cbb6181f2eda55e8f63895105
estevaoMG <estevao.gouveia@hotmail.com>
2025-10-20 20:25:32 -0300

    Utilização do Docker Compose e configuração do Alembic

--- Makefile
+++ Makefile
@@ -1,2 +1,8 @@
 run:
 	@uvicorn main:app --reload
+
+create-migrations:
+	@pythonpath=$pythonpath:$(pwd) alembic revision --autogenerate -m $(d)
+
+run-migrations:
+	@pythonpath=$pythonpath:$(pwd) alembic upgrade head
--- atleta/models.py
+++ atleta/models.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sqlalchemy import Datetime, Float, ForeignKey, Integer, String
+from sqlalchemy import DateTime, Float, ForeignKey, Integer, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from contrib.models import BaseModel
@@ -11,15 +11,17 @@ class AtletaModel(BaseModel):
 
     pk_id: Mapped[int] = mapped_column(Integer, primary_key=True)
     nome: Mapped[str] = mapped_column(String(50), nullable=False)
-    cpf: Mapped[str] = mapped_column(String(11), nullable=False)
+    cpf: Mapped[str] = mapped_column(String(11), unique=True, nullable=False)
     idade: Mapped[int] = mapped_column(Integer, nullable=False)
     peso: Mapped[float] = mapped_column(Float, nullable=False)
     altura: Mapped[float] = mapped_column(Float, nullable=False)
     sexo: Mapped[str] = mapped_column(String(1), nullable=False)
-    created_at: Mapped[datetime] = mapped_column(Datetime, nullable=False)
+    created_at: Mapped[datetime] = mapped_column(DateTime, nullable=False)
     centro_treinamento: Mapped["CentroTreinamentoModel"] = relationship(
         back_populates="atleta"
     )
     centro_treinamento_id: Mapped[int] = mapped_column(
         Integer, ForeignKey("centros_treinamento.pk_id")
     )
+    categoria: Mapped["CategoriaModel"] = relationship(back_populates="atleta")
+    categoria_id: Mapped[int] = mapped_column(Integer, ForeignKey("categorias.pk_id"))
