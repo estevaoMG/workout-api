487d3f7565ef712a61102fd7bab995234d57b9dd
estevaoMG <estevao.gouveia@hotmail.com>
2025-11-03 18:37:16 -0300

    Criação das rotas de Centro de Treinamento

--- centro_treinamento/schemas.py
+++ centro_treinamento/schemas.py
@@ -1,11 +1,11 @@
 from typing import Annotated
 
-from pydantic import Field
+from pydantic import UUID4, Field
 
 from contrib.schemas import BaseSchema
 
 
-class CentroTreinamento(BaseSchema):
+class CentroTreinamentoIn(BaseSchema):
     nome: Annotated[
         str,
         Field(
@@ -28,5 +28,25 @@ class CentroTreinamento(BaseSchema):
             description="Proprietário do centro de treinamento",
             example="Marcos",
             max_length=30,
+        ),
+    ]
+
+
+class CentroTreinamentoAtleta(BaseSchema):
+    nome: Annotated[
+        str,
+        Field(
+            description="Nome do centro de treinamento",
+            example="CT King",
+            max_length=20,
+        ),
+    ]
+
+
+class CentroTreinamentoOut(CentroTreinamentoIn):
+    id: Annotated[
+        UUID4,
+        Field(
+            description="Identificador do centro de treinamento",
         ),
     ]
